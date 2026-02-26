73b70425e583ec18e65732c9bb2695e39555a2a6
japanese-cheat-sheet <james.urron+1@gmail.com>
2026-02-26 15:53:46 +0000

    better CSS varibles + Side menu test

--- colors.js
+++ colors.js
@@ -12,20 +12,11 @@ if(
 console.log(window.localStorage.getItem("PRIMARY"))
 console.log(window.localStorage.getItem("SECONDARY"))
 
-var r = document.querySelector(':root');
 function setColors(array){
-    var primary = ["--background", "--table-background", "--popup-backround",  "--select-text-color"]
-    var secondary = ["--background2", "--heading-text-color", "--text-color", "--table-border-color", "--table-text-color", "--popup-text-color", "--popup-border-color","--select-backround","--border-color"]
-    for(i=0;i<primary.length;i++){
-        if(primary[i] == "--table-background") {
-            r.style.setProperty(primary[i],array[0]+"AF")
-        } else {
-            r.style.setProperty(primary[i],array[0])
-        }
-    }
-    for(i=0;i<secondary.length;i++){
-        r.style.setProperty(secondary[i],array[1])
-    }
+    var r = document.querySelector(':root');
+    r.style.setProperty("--PRIMARY",array[0])
+    r.style.setProperty("--PRIMARYalpha",array[0]+"AF")
+    r.style.setProperty("--SECONDARY",array[1])
 }
 setColors([window.localStorage.getItem("PRIMARY"),window.localStorage.getItem("SECONDARY")])
 
