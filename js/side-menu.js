9c4d38ca8074655596f59eaa9697bda020366ecb
japanese-cheat-sheet <james.urron+1@gmail.com>
2026-02-27 01:27:01 +0000

    content menu scroll updated

--- js/side-menu.js
+++ js/side-menu.js
@@ -7,14 +7,13 @@ if(window.matchMedia("(pointer: coarse)").matches == false) {
     })
 }
 
-function scrollById(Name){
-    var elemRect = document.getElementById(Name).getBoundingClientRect()
-    if(window.matchMedia("(pointer: coarse)").matches == false) {
-        window.scrollTo(0,elemRect.top + window.scrollY - 125 )
-    } else {
-        window.scrollTo(0,elemRect.top + window.scrollY - 65 )
-    }
-    
+function scrollById(event,Name){
+    console.log(event.srcElement.getBoundingClientRect())
+    var menuA = event.srcElement.getBoundingClientRect()
+    var elem = document.getElementById(Name)
+    var elemRect = elem.getBoundingClientRect()
+    var topPadding = Number(window.getComputedStyle(elem, null).getPropertyValue('padding-top').replace("px",""))
+    window.scrollTo(0,elemRect.top + window.scrollY - menuA.top + menuA.height/2 + (topPadding/2 * window.devicePixelRatio))
 }
 
 setTimeout(function(){
@@ -23,6 +22,7 @@ setTimeout(function(){
     var menu = document.getElementById("side-menu")
 
     button.addEventListener("click",function(){
+        console.log(menu.scrollTop)
         if(sideToggle){
             button.innerText = "☰"
             button.style.fontWeight = "400"
@@ -38,17 +38,17 @@ setTimeout(function(){
 
     var list = [
         ["日本語（ＪＡＰＡＮＥＳＥ）","window.scrollTo(0,0)", 1],
-        ["平仮名（ＨＩＲＡＧＡＮＡ）","scrollById('hiragana')", 1],
-        ["片仮名（ＫＡＴＡＫＡＮＡ）","scrollById('katakana')", 1],
-        ["助詞（ＰＡＲＴＩＣＬＥＳ）","scrollById('particle-title')", 1],
-        ["格助詞（ＣＡＳＥ　ＰＡＲＴＩＣＬＥＳ）","scrollById('case-particles')",2],
-        ["並立助詞（ＰＡＲＡＬＬＥＬ　ＰＡＲＴＩＣＬＥＳ）","scrollById('parallel-particles')",2],
-        ["終助詞（ＳＥＮＴＥＮＣＥ－ＥＮＤＩＮＧ　ＰＡＲＴＩＣＬＥＳ）","scrollById('sentence-ending-particles')",2],
-        ["間投助詞（ＩＮＴＥＲＪＥＣＴＯＲＹ　ＰＡＲＴＩＣＬＥＳ）","scrollById('interjectory-particles')",2],
-        ["副助詞（ＡＤＶＥＲＢＩＡＬ　ＰＡＲＴＩＣＬＥＳ）","scrollById('adverbial-particles')",2],
-        ["係助詞（ＢＩＮＤＩＮＧ　ＰＡＲＴＩＣＬＥＳ）","scrollById('binding-particles')",2],
-        ["接続助詞（ＣＯＮＪＵＮＣＴＩＶＥ　ＰＡＲＴＩＣＬＥＳ）","scrollById('conjunctive-particles')",2],
-        ["準体助詞（ＮＯＲＭＡＬＩＳＩＮＧ　ＰＡＲＴＩＣＬＥＳ）","scrollById('normalising-particles')",2]
+        ["平仮名（ＨＩＲＡＧＡＮＡ）","scrollById(event,'hiragana')", 1],
+        ["片仮名（ＫＡＴＡＫＡＮＡ）","scrollById(event,'katakana')", 1],
+        ["助詞（ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'particle-title')", 1],
+        ["格助詞（ＣＡＳＥ　ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'case-particles')",2],
+        ["並立助詞（ＰＡＲＡＬＬＥＬ　ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'parallel-particles')",2],
+        ["終助詞（ＳＥＮＴＥＮＣＥ－ＥＮＤＩＮＧ　ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'sentence-ending-particles')",2],
+        ["間投助詞（ＩＮＴＥＲＪＥＣＴＯＲＹ　ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'interjectory-particles')",2],
+        ["副助詞（ＡＤＶＥＲＢＩＡＬ　ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'adverbial-particles')",2],
+        ["係助詞（ＢＩＮＤＩＮＧ　ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'binding-particles')",2],
+        ["接続助詞（ＣＯＮＪＵＮＣＴＩＶＥ　ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'conjunctive-particles')",2],
+        ["準体助詞（ＮＯＲＭＡＬＩＳＩＮＧ　ＰＡＲＴＩＣＬＥＳ）","scrollById(event,'normalising-particles')",2]
     ]
 
     function addContents(array){
